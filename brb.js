ebfeb29ee4cb214bcb2e77eaaa5246f00b471293
Marisa Marion Mackenzie <82250600+marisaintheflesh@users.noreply.github.com>
2024-01-07 12:18:40 -0500

    Clean up some code, returned random start point

--- brb.js
+++ brb.js
@@ -3,13 +3,13 @@ function random(max)
 	return Math.floor(Math.random() * max);
 }
 
-var ww;
-var hh; 
+var canvasWidth = 640;
+var canvasHeight = 360;
 var w = 102;
 var h = 13;
 
-var x = 1;
-var y = 1;
+var x = random(canvasWidth - w - 1);
+var y = random(canvasHeight - h - 1); // < ^ the '- 1' is important, otherwise the image could potentially go in a straight line instead of diagonally
 var timestamp = function() { return Date.now(); }
 var fps=60;
 var interval;
@@ -21,10 +21,8 @@ function init()
 {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
-  canvas.width = 640;
-  canvas.height = 360;
-  ww = canvas.width = canvas.offsetWidth;
-  hh = canvas.height = canvas.offsetHeight;
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
   brb.src = "brb.png";
   interval=1000/fps;
   then=Date.now();
@@ -50,17 +48,17 @@ function draw(timestamp) {
 	  then = now - (elapsed % interval);
 	  
 	  ctx.fillStyle = 'rgba(255,255,0,0.0)';
-	  ctx.clearRect(0,0,ww,hh); // clear canvas
+	  ctx.clearRect(0,0,canvasWidth,canvasHeight); // clear canvas
 	  ctx.save();
 	  
 	  x += xspeed;
 	  y += yspeed;
-	  if((x + w) >= ww || x <= 0)
+	  if((x + w) >= canvasWidth || x <= 0)
 	  {
 		xspeed = -xspeed;
 	  }
 
-	  if((y + h) >= hh || y <= 0)
+	  if((y + h) >= canvasHeight || y <= 0)
 	  {
 		yspeed = -yspeed;
 	  }
